39cc355c738e42a79549da9652217ad075cc92f6
Galen Emery <galen@chef.io>
2017-09-13 19:45:55 +0000

    added workspace/.ssh directory
    Signed-off-by: Galen Emery <galen@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,9 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+directory '/var/opt/delivery/workspace/.ssh' do
+  recursive true
+  owner 'dbuild'
+end
+
 cookbook_file '/var/opt/delivery/workspace/.ssh/galen_sa_west_2.pem' do
   source 'galen_sa_west_2.pem'
   mode 0400
+  owner 'dbuild'
 end
 
 include_recipe 'delivery-truck::default'
