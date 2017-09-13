58645d49ab409c189dcedc086904e75688e4c69d
Galen Emery <galen@chef.io>
2017-09-13 19:41:28 +0000

    added train/ec2 key
    Signed-off-by: Galen Emery <galen@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,4 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+cookbook_file '/var/opt/delivery/workspace/.ssh/galen_sa_west_2.pem' do
+  source 'galen_sa_west_2.pem'
+  mode 0400
+end
+
 include_recipe 'delivery-truck::default'
--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -3,4 +3,6 @@
 # Recipe:: deploy
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+node.default['delivery']['config']['delivery-truck']['deploy']['search'] = 'tags:canary'
+
 include_recipe 'delivery-truck::deploy'
